e450bf74f603a3b6c8f5e426a889dbbc570b523e
kashitakashi <kashi_takashi@twilightseed.com>
2023-09-28 19:29:16 -0500

    Create LXCinstaller.sh
    Base code for automatic install on LXC containers

---
+++ LXCinstaller.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+apt update
+apt dist-upgrade -y
+apt install git -y
+
+wget https://go.dev/dl/go1.21.1.linux-amd64.tar.gz
+
+tar -C /usr/local -xzf go1.21.1.linux-amd64.tar.gz
+
+export PATH=$PATH:/usr/local/go/bin
+
+mkdir /CheesyArena
+cd /CheesyArena
+git clone https://github.com/Test2220/cheesy-arena-2220.git /CheesyArena
+go build
+systemctl link /CheesyArena/Cheesy-arena.service
